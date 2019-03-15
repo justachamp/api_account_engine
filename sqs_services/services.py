8931042dce64d2aee187a2404d573a1b25595600
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-03-15 19:55:37 -0300

    AWS SQS Services

---
+++ sqs_services/services.py
@@ -0,0 +1,25 @@
+import boto3
+from rest_framework.utils import json
+from django.conf import settings
+
+
+class SqsService:
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def push(self, queue_name):
+        sqs = boto3.resource('sqs',
+                             region_name=settings.AWS_REGION_NAME,
+                             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+        url = f"{settings.TREASURY_SQS_URL}{queue_name}"
+        msg = json.dumps(self.json_data)
+        queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+        try:
+            queue.send_message(QueueUrl=url, MessageBody=msg)
+            return True
+
+        except Exception as exp:
+            raise ValueError('Could not send message to SQS')
